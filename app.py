8427549f1a668f140961216b2148e69a31d8a389
CamiloOlivares <colivaresa@alumnosuls.cl>
2021-04-07 13:08:32 -0400

    first

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,jsonify,request
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+cors = CORS(app)
+
+from pokemons import pokemons
+from pokemons import perfil
+from pokemons import horario
+
+@app.route('/ping')
+def ping():
+    return jsonify({"ok":True,"message":"hola"})
+
+@app.route('/pokemones')
+def pokemones():
+    return jsonify({"ok":True,"pokemons":pokemons}),200
+
+@app.route('/cambiarPokemon',methods=['POST'])
+def cambiarPokemon():
+    print(request.get_data())
+    # return jsonify({"ok":True,"message":"Pokemon ingresado correctamente","pokemons":pokemons})
+    return jsonify({"data":request.form.getlist('tamano')})
+
+@app.route('/returnput',methods=['PUT'])
+def returnput():
+    return jsonify({"ok":True,"message":"este es un put"}) 
+
+@app.route('/getPerfil',methods=['POST'])
+def getperfil():
+    return jsonify(perfil[0])
+
+@app.route('/getHorario',methods=['POST'])
+def gethorario():
+    return jsonify(horario)     
+       
+if __name__=='__main__':
+    app.run(debug=True, port=4000,host='192.168.0.57')
+
+
---
+++ pokemons.py
@@ -0,0 +1,57 @@
+pokemons = [
+    {"name":"bulbasaur","tipo":"planta","tamano":5,"peso":0.5},
+    {"name":"squirtle","tipo":"agua","tamano":6,"peso":0.45},
+    {"name":"charmander","tipo":"fuego","tamano":6,"peso":0.3}
+]
+
+perfil = [
+    {
+        "Correo":"camilo@camilo.cl",
+        "Rut":"19.492.582-4",
+        "Nombre":"Camilo",
+        "ApellidoP":"Olivares",
+        "ApellidoM":"Alvarado",
+        "Telefono":"123456789",
+        "Direccion":"Hola 123"
+
+    }
+]
+
+horario = [
+    {
+"Dia": "Miercoles",
+"Horario_Inicio": "16:15:00",
+"ID_Sala": "03-ind",
+"Nombre": "Teor\u00eda de Aut\u00f3matas y Lenguajes Formales - T"
+},
+{
+"Dia": "Miercoles",
+"Horario_Inicio": "16:15:00",
+"ID_Sala": "210D",
+"Nombre": "Bases de Datos I - T"
+},
+{
+"Dia": "Martes",
+"Horario_Inicio": "09:45:00",
+"ID_Sala": "201D",
+"Nombre": "Ingl\u00e9s I - T"
+},
+{
+"Dia": "Lunes",
+"Horario_Inicio": "14:30:00",
+"ID_Sala": "209D",
+"Nombre": "Bases de Datos I - T"
+},
+{
+"Dia": "Jueves",
+"Horario_Inicio": "09:45:00",
+"ID_Sala": "202D",
+"Nombre": "Ingl\u00e9s I - T"
+},
+{
+"Dia": "Martes",
+"Horario_Inicio": "09:45:00",
+"ID_Sala": "111M",
+"Nombre": "Investigaci\u00f3n de Operaciones I - T"
+}
+]
